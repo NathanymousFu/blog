55e85cef47b4166300d0b6055cf911f70cb60560
huhinka <huhinka@163.com>
2024-07-18 13:14:55 +0800

    refact: extract PostItem component

---
+++ app/PostItem.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import Link from '@/components/Link'
+import Tag from '@/components/Tag'
+import siteMetadata from '@/data/siteMetadata'
+import { formatDate } from 'pliny/utils/formatDate'
+
+export default function PostItem({ date, slug, title, tags, summary }) {
+  return (
+    <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+      <dl>
+        <dt className="sr-only">Published on</dt>
+        <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+          <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+        </dd>
+      </dl>
+      <div className="space-y-5 xl:col-span-3">
+        <div className="space-y-6">
+          <div>
+            <h2 className="text-2xl font-bold leading-8 tracking-tight">
+              <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+                {title}
+              </Link>
+            </h2>
+            <div className="flex flex-wrap">
+              {tags.map((tag) => (
+                <Tag key={tag} text={tag} />
+              ))}
+            </div>
+          </div>
+          <div className="prose max-w-none text-gray-500 dark:text-gray-400">{summary}</div>
+        </div>
+        <div className="text-base font-medium leading-6">
+          <Link
+            href={`/blog/${slug}`}
+            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            aria-label={`Read more: "${title}"`}
+          >
+            Read more &rarr;
+          </Link>
+        </div>
+      </div>
+    </article>
+  )
+}
