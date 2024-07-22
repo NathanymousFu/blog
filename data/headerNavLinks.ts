1af6e2bd0e32d3029dada060af118aa84346cd2c
huhinka <huhinka@163.com>
2024-07-22 21:42:08 +0800

    make project header link disappear automatically when no projects available

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,10 +1,14 @@
+import projectsData from './projectsData'
+
 const headerNavLinks = [
   { href: '/', title: '主页' },
   { href: '/blog', title: '博客' },
   { href: '/tags', title: '标签' },
-  // TODO uncomment this line after adding a projects
-  // { href: '/projects', title: '项目' },
   { href: '/about', title: '关于' },
 ]
+
+if (projectsData.length > 0) {
+  headerNavLinks.splice(3, 0, { href: '/projects', title: '项目' })
+}
 
 export default headerNavLinks
