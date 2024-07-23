dcf6c7d228e3883af27e6054fd9fb3ff4773b7cd
huhinka <huhinka@163.com>
2024-07-23 09:55:51 +0800

    scroll to welcome section automatically on page load

--- app/Welcome.tsx
+++ app/Welcome.tsx
@@ -1,6 +1,16 @@
+'use client'
+
+import { useEffect, useRef } from 'react'
+
 export default function Welcome() {
+  const refContainer = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    // @ts-ignore
+    refContainer.current?.scrollIntoView({ behavior: 'smooth' })
+  }, [])
+
   return (
-    <section className="welcome-container">
+    <section className="welcome-container" ref={refContainer}>
       <div className="welcome-div">
         <h1 className="welcome-big-title">
           <span className="welcome-text">欢迎</span>来到我的空间，我是一名 TS 全栈开发者{' '}
