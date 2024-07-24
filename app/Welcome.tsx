d296a705a4ceacba7c862ab671fc92113387a313
huhinka <huhinka@163.com>
2024-07-24 11:47:09 +0800

    adapt the welcome line to dark mode

--- app/Welcome.tsx
+++ app/Welcome.tsx
@@ -1,13 +1,19 @@
 'use client'
 
+import { clsx } from 'clsx'
+import { useTheme } from 'next-themes'
 import { useEffect, useRef } from 'react'
 
 export default function Welcome() {
+  const { theme } = useTheme()
   const refContainer = useRef<HTMLDivElement>(null)
+
   useEffect(() => {
     // @ts-ignore
     refContainer.current?.scrollIntoView({ behavior: 'smooth' })
   }, [])
+
+  const welcomeLineClass = clsx('welcome-line', theme === 'dark' && 'welcome-line-dark')
 
   return (
     <section className="welcome-container" ref={refContainer}>
@@ -16,7 +22,7 @@ export default function Welcome() {
           <span className="welcome-text">欢迎</span>来到我的空间，我是一名 TS 全栈开发者{' '}
         </h1>
         <div className="welcome-research">下拉了解更多</div>
-        <div className="welcome-line" />
+        <div className={welcomeLineClass} />
       </div>
     </section>
   )
