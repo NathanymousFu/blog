7ac37abe80eb1b2a8707149405ef36b8713cd3d0
huhinka <huhinka@163.com>
2025-03-22 12:18:11 +0800

    change Java to TS

--- app/Welcome.tsx
+++ app/Welcome.tsx
@@ -24,7 +24,7 @@ export default function Welcome() {
     <section className="welcome-container" ref={refContainer}>
       <div className="welcome-div">
         <h1 className="welcome-big-title">
-          <span className="welcome-text">欢迎</span>来到我的空间，我是一名 Java 全栈开发者{' '}
+          <span className="welcome-text">欢迎</span>来到我的空间，我是一名 TS 全栈开发者{' '}
         </h1>
         <div className="welcome-research">下拉了解更多</div>
         <div className={welcomeLineClass} />
